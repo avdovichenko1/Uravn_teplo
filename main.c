5b6b0e0e077b68b4326408c1248d09c8cf5e1ae3
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-03-07 17:38:34 +0700

    Update main.c

--- main.c
+++ main.c
@@ -31,18 +31,24 @@ int main(int argc, char *argv[]) {
         temp = (arr_pred[raz - 1][raz - 1] - arr_pred[0][raz - 1]) / (raz - 1);
         arr_pred[j][raz - 1] = temp + arr_pred[j - 1][raz - 1];
     }
-    int num_iter = 0;
-    double error = max_toch + 1;
 #pragma acc data copy(arr_pred[:raz][:raz]) create(arr_new[:raz][:raz])
     {
-        while(max_num_iter > num_iter && max_toch < error){
+        int tile_size = 16; // adjust this value for best performance
+        double error = max_toch + 1;
+        int num_iter = 0;
+        while (max_num_iter > num_iter && max_toch < error) {
             error = 0;
 #pragma acc parallel loop reduction(max:error)
-            for(int j = 1; j < raz - 1; j++) {
-#pragma acc loop reduction(max:error)
-                for(int i = 1; i < raz - 1; i++){
-                    arr_new[i][j] = (arr_pred[i-1][j] + arr_pred[i][j-1] + arr_pred[i][j+1]+arr_pred[i+1][j])*0.25;
-                    error = fmax(fabs( arr_pred[i][j]-arr_new[i][j]), error);
+            for (int tile_j = 1; tile_j < raz - 1; tile_j += tile_size) {
+#pragma acc loop independent reduction(max:error)
+                for (int tile_i = 1; tile_i < raz - 1; tile_i += tile_size) {
+                    for (int j = tile_j; j < fmin(tile_j + tile_size, raz - 1); j++) {
+#pragma acc loop vector independent
+                        for (int i = tile_i; i < fmin(tile_i + tile_size, raz - 1); i++) {
+                            arr_new[j][i] = (arr_pred[j-1][i] + arr_pred[j][i-1] + arr_pred[j][i+1] + arr_pred[j+1][i]) * 0.25;
+                            error = fmax(fabs(arr_pred[j][i] - arr_new[j][i]), error);
+                        }
+                    }
                 }
             }
 #pragma acc parallel loop
@@ -54,9 +60,8 @@ int main(int argc, char *argv[]) {
             }
             num_iter++;
         }
+        printf("Programms result: %d, %0.6lf\n", num_iter, error);
     }
-
-    printf("Programms result: %d, %0.6lf\n", num_iter, error);
     clock_t b=clock();
     double d=(double)(b-a)/CLOCKS_PER_SEC;
     printf("%.25f time in sec", d);
