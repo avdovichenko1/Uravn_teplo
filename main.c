9acf81079e71a5d15f6ffabe3ef154a8253e0f9a
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-03-07 23:39:05 +0700

    Update main.c

--- main.c
+++ main.c
@@ -19,9 +19,10 @@ int main(int argc, char *argv[]) {
     arr_pred[0][raz - 1] = 20;
     arr_pred[raz - 1][raz - 1] = 20;
     arr_pred[raz - 1][0] = 30;
-#pragma acc data copyin(arr_pred[:raz][:raz]) copyout(arr_new[:raz][:raz])
+
+#pragma acc data copy(arr_pred[:raz][:raz]) create(arr_new[:raz][:raz])
     {
-#pragma acc parallel loop gang(32) vector(32)
+#pragma acc parallel loop
         for(int i = 1; i < raz - 1; i++){
             double temp = (arr_pred[0][raz - 1] - arr_pred[0][0]) / (raz - 1);
             arr_pred[0][i] = temp + arr_pred[0][i - 1];
@@ -34,35 +35,24 @@ int main(int argc, char *argv[]) {
         }
         int num_iter = 0;
         double error = max_toch + 1;
-#pragma acc data copyin(arr_pred[:raz][:raz]) copyout(arr_new[:raz][:raz])
+#pragma acc data copy(arr_pred[:raz][:raz]) create(arr_new[:raz][:raz])
         {
             while (max_num_iter > num_iter && max_toch < error) {
                 error = 0;
-#pragma acc parallel loop gang(32) vector(32) reduction(max:error)
+#pragma acc parallel loop reduction(max:error)
                 for (int i = 1; i < raz - 1; i++) {
-#pragma acc loop gang(32) vector(32) reduction(max:error)
+#pragma acc loop reduction(max:error)
                     for (int j = 1; j < raz - 1; j++) {
                         arr_new[i][j] =
                                 (arr_pred[i - 1][j] + arr_pred[i][j - 1] + arr_pred[i][j + 1] + arr_pred[i + 1][j]) *
                                 0.25;
-                        error = fmax(fabs(arr_pred[i][j] - arr_new[i][j]), error);
-                    }
-                }
-#pragma acc parallel loop gang(32) vector(32)
-                for (int j = 1; j < raz - 1; j++) {
-#pragma acc loop gang(32) vector(32)
-                    for (int i = 1; i < raz - 1; i++) {
-                        arr_pred[j][i] = arr_new[j][i];
-                    }
+                        error = fmax(fabs(arr_pred[i][j] - arr_new[i][j]), error);}
                 }
                 num_iter++;
             }
         }
         printf("Programms result: %d, %0.6lf\n", num_iter, error);
     }
-
-
-
     clock_t b=clock();
     double d=(double)(b-a)/CLOCKS_PER_SEC;
     printf("%.25f time in sec", d);
