227856a28bb7493ea86a3dda84e04c9b46af484b
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-04-21 17:54:51 +0700

    Update lab4.cu

--- lab4.cu
+++ lab4.cu
@@ -48,7 +48,7 @@ __global__ void reduceError(double* tol1, double* tolbl, int N){
         i += global_size;
     }
     extern __shared__ double shared_array[]; //объявляется внешняя область памяти
-    shared_array[thread_id] = tol; // значение tol сохраняется в общей памяти
+    shared_array[thread_id] = tol;
     __syncthreads(); //выполняется синхронизация потоков, чтобы убедиться, что все потоки закончили запись в общую память
     int size = blockDim.x / 2;
     while (size > 0){
@@ -91,23 +91,27 @@ int main(int argc, char* argv[]) {
     double *arr_pred = (double*)malloc((size + 2) * (size + 2) * sizeof(double));
     double *arr_new = (double*)malloc((size + 2) * (size + 2) * sizeof(double));
 
+    int num_iter = 0;
+    double error = 1.0;
+    double shag = 10.0 / (size + 2);
+
     int size_pot=32; // количество потоков
     dim3 Block_size(size_pot, size_pot, 1); //размер блока и определение количества потоков в каждом блоке, 1 блок - 1024 потока
-    dim3 Grid_Size((size + 33)/size_pot, (size + 33)/size_pot, 1);
+    dim3 Grid_Size((size + 33)/size_pot, (size + 33)/size_pot, 1); // определяет количество блоков в сетке, рассчитывается
+    // на основе размера матрицы size и используется для распределения блоков на сетке таким образом, чтобы охватить
+    // всю матрицу и иметь достаточное количество блоков для выполнения параллельных вычислений на GPU
 
-    double* d_A, *d_Anew;
-    cudaMalloc((void**)&d_A, sizeof(double ) *(size+ 2)*(size+2));
-    cudaMalloc((void**)&d_Anew, sizeof(double ) *(size+ 2)*(size+2));
-    int iter_host = 0;
-    double error_host = 1.0;
-    double add_grad_host = 10.0 / (size + 2);
+    double* arr_pred_gp, *arr_new_gp;
+    cudaMalloc((void**)&arr_pred_gp, sizeof(double ) * (size + 2) * (size + 2));
+    cudaMalloc((void**)&arr_new_gp, sizeof(double ) * (size + 2) * (size + 2));
 
     int len_host = size + 2;
+
     for (int i = 0; i < size + 2; i++){
-        arr_pred[i * len_host] = 10 + add_grad_host * i;
-        arr_pred[i] = 10 + add_grad_host * i;
-        arr_pred[len_host * (size + 1) + i] = 20 + add_grad_host * i;
-        arr_pred[len_host * i + size + 1] = 20 + add_grad_host * i;
+        arr_pred[i * len_host] = 10 + shag * i;
+        arr_pred[i] = 10 + shag * i;
+        arr_pred[len_host * (size + 1) + i] = 20 + shag * i;
+        arr_pred[len_host * i + size + 1] = 20 + shag * i;
 
         arr_new[len_host * i] = arr_pred[i * len_host];
         arr_new[i] = arr_pred[i];
@@ -116,8 +120,8 @@ int main(int argc, char* argv[]) {
     }
 
 
-    cudaMemcpy(d_A, arr_pred, sizeof(double) * (size + 2) * (size + 2), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_Anew, arr_new, sizeof(double) * (size + 2) * (size + 2), cudaMemcpyHostToDevice);
+    cudaMemcpy(arr_pred_gp, arr_pred, sizeof(double) * (size + 2) * (size + 2), cudaMemcpyHostToDevice);
+    cudaMemcpy(arr_new_gp, arr_new, sizeof(double) * (size + 2) * (size + 2), cudaMemcpyHostToDevice);
 
     double* d_err_1d;
     cudaMalloc(&d_err_1d, sizeof(double) * (size * size));
@@ -132,26 +136,26 @@ int main(int argc, char* argv[]) {
     cudaMalloc((void **)(&d_ptr), sizeof(double));
 
     cudaDeviceSynchronize();
-    while ((error_host > tol) && (iter_host < iter_max)){
-        iter_host++;
-        if ((iter_host % 150 == 0) || (iter_host == 1)){
-            error_host = 0.0;
-            updateError<<<Grid_Size, Block_size>>>(d_A, d_Anew, size, error_host, d_err_1d);
+    while ((error > tol) && (num_iter < iter_max)){
+        num_iter++;
+        if ((num_iter % 150 == 0) || (num_iter == 1)){
+            error = 0.0;
+            updateError<<<Grid_Size, Block_size>>>(arr_pred_gp, arr_new_gp, size, error, d_err_1d);
             reduceError<<<errGS, errBS, (errBS.x) * sizeof(double)>>>(d_err_1d, dev_out, size * size);
             reduceError<<<1, errBS, (errBS.x) * sizeof(double)>>>(dev_out, d_err_1d, errGS.x);
-            cudaMemcpy(&error_host, &d_err_1d[0], sizeof(double), cudaMemcpyDeviceToHost);
+            cudaMemcpy(&error, &d_err_1d[0], sizeof(double), cudaMemcpyDeviceToHost);
         }
         else
-            updateTemperature<<<Grid_Size, Block_size>>>(d_A, d_Anew, size);
-        d_ptr = d_A;
-        d_A = d_Anew;
-        d_Anew = d_ptr;
-        if ((iter_host % 150 == 0) || (iter_host == 1)){
-            printf("%d : %lf\n", iter_host, error_host);
+            updateTemperature<<<Grid_Size, Block_size>>>(arr_pred_gp, arr_new_gp, size);
+        d_ptr = arr_pred_gp;
+        arr_pred_gp = arr_new_gp;
+        arr_new_gp = d_ptr;
+        if ((num_iter % 150 == 0) || (num_iter == 1)){
+            printf("%d : %lf\n", num_iter, error);
             fflush(stdout);
         }
     }
-    printf("%d : %lf\n", iter_host, error_host);
+    printf("%d : %lf\n", num_iter, error);
     clock_t b=clock();
     double d=(double)(b-a)/CLOCKS_PER_SEC; // переводит в секунды
     printf("%.25f время в секундах", d);
