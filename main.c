6365eb0505f34a4c7ad8511eb5653876c5f754c3
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-04-21 18:57:16 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,150 +1,163 @@
-#include <cublas_v2.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <cstdlib>
+#include <cstdio>
+#include <malloc.h>
 #include <time.h>
 
+#define max(x, y) ((x) > (y) ? (x) : (y))
 
-int main(int argc, char *argv[]) {
-    int max_num_iter;
-    int raz;
-    double max_toch;
+
+__global__ void updateTemperature(const double* arr_pred, double* arr_new, int N){
+    //Индекс j вычисляется как произведение номера блока по вертикальной оси (blockIdx.y) на размер блока по вертикальной оси
+    // (blockDim.y),плюс номер потока внутри блока по вертикальной оси (threadIdx.y), что позволяет потокам различных блоков
+    // и потокам внутри одного блока работать с различными строками массива данных, i - аналогично.
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+//проверяется, что индексы j и i находятся внутри диапазона от 1 до N + 1, чтобы исключить обработку граничных элементов массива.
+    if (j > 0 && j < N + 1)
+        if (i > 0 && i < N + 1)
+            //новое значение элемента массива arr_new[j * (N + 2) + i] вычисляется на основе предыдущего состояния массива
+            // arr_pred,используя формулу теплопроводности
+            arr_new[j * (N + 2) + i] = 0.25 * (arr_pred[(j + 1) * (N + 2) + i] + arr_pred[(j - 1) * (N + 2) + i] +
+                                               arr_pred[j * (N + 2) + i - 1] + arr_pred[j * (N + 2) + i + 1]);
+}
+
+__global__ void updateError(const double* arr_pred, double* arr_new, int N, double tol, double* tol1){
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (j > 0 && j < N + 1)
+        if (i > 0 && i < N + 1) {
+            arr_new[j * (N + 2) + i] = 0.25 * (arr_pred[(j + 1) * (N + 2) + i] + arr_pred[(j - 1) * (N + 2) + i] + arr_pred[j * (N + 2) + i - 1] + arr_pred[j * (N + 2) + i + 1]);
+            //Вычисление значения погрешности между новым значением элемента и соответствующим предыдущим значением элемента
+            tol1[i * j - 1] = max(arr_new[j * (N + 2) + i] - arr_pred[j * (N + 2) + i], tol);
+        };
+}
+
+__global__ void reduceError(double* tol1, double* tolbl, int N){
+    int thread_id = threadIdx.x; // индекс текущего потока внутри блока
+    int global_size = blockDim.x * gridDim.x;  //вычисляет общее количество потоков на сетке ( путем умножения количества
+    // потоков в блоке (blockDim.x) на количество блоков в сетке (gridDim.x))
+    int global_id = blockDim.x * blockIdx.x + threadIdx.x; //вычисляет глобальный индекс текущего потока (включает в
+    // себя индекс блока и индекс потока внутри блока)
+    double tol = tol1[0];
+    int i  = global_id;
+    while(i < N){
+        tol = max(tol, tol1[i]);
+        i += global_size;
+    }
+    extern __shared__ double shared_array[]; //объявляется внешняя область памяти
+    shared_array[thread_id] = tol;
+    __syncthreads(); //выполняется синхронизация потоков, чтобы убедиться, что все потоки закончили запись в общую память
+    int size = blockDim.x / 2;
+    while (size > 0){
+        if (size > thread_id)
+            shared_array[thread_id] = max(shared_array[thread_id + size], shared_array[thread_id]);
+        __syncthreads();
+        size /= 2;
+    }
+
+    if (thread_id == 0)
+        tolbl[blockIdx.x] = shared_array[0]; // значение максимальной ошибки сохраняется только в одном потоке с индексом 0 внутри блока
+}
+
+
+int main(int argc, char* argv[]) {
+    clock_t a=clock();
+    int size;
+    double tol;
+    int iter_max;
     if (argc < 4){
-        printf("Not enough args");
+        printf("Неправильное количество аргументов");
         exit(1);
     }
-    else
-    {
-        max_num_iter = atoi(argv[1]); // количество итераций
-        if (max_num_iter == 0){
-            printf("incorrect first param");
-            exit(1);
-        }
-        max_toch = atof(argv[2]); // точность
-        if (max_toch == 0){
-            printf("incorrect second param");
-            exit(1);
-        }
-        raz = atoi(argv[3]); // размер сетки
-        if (raz == 0){
-            printf("incorrect third param");
-            exit(1);
-        }
+    tol = strtod(argv[1], NULL);
+    if (tol <= 0){
+        printf("Ограничение точности должно превышать 0");
+        exit(1);
     }
-   
-    clock_t a=clock();
+    size = atoi(argv[2]);
+    if (size <= 0){
+        printf("Размер матриццы должен быть больше 0");
+        exit(1);
+    }
+    iter_max = atoi(argv[3]);
+    if (iter_max <= 0){
+        printf("Максимальное количество итераци должно быть больше 0");
+        exit(1);
+    }
 
-    double* arr_pred = (double*)calloc(raz * raz, sizeof(double));
-    double* arr_new = (double*)calloc(raz * raz, sizeof(double));
-
-    arr_pred[0] = 10;
-    arr_pred[raz-1] = 20;
-    arr_pred[raz * (raz - 1) +raz - 1] = 30;
-    arr_pred[raz * (raz-1)] = 20;
+    double *arr_pred = (double*)malloc((size + 2) * (size + 2) * sizeof(double));
+    double *arr_new = (double*)malloc((size + 2) * (size + 2) * sizeof(double));
 
     int num_iter = 0;
-    double error = 1 + max_toch;
-    double shag = (10.0 / (raz - 1));
-    
-// выделение памяти на устройстве и копирование данных из памяти хоста в память устройства
-#pragma acc enter data create(arr_pred[0:raz*raz], arr_new[0:raz*raz]) copyin(raz, shag)
-    
-// ядро, выполняющее циклическое заполнение массива arr_pred
-#pragma acc kernels
-    {
-#pragma acc loop independent
-        for (int j = 0; j < raz; j++) {
-            arr_pred[j] = 10 + j * (10.0 / (raz - 1));
-            arr_pred[j * raz] = 10 + j * (10.0 / (raz - 1));
-            arr_pred[(raz - 1) * raz + j] = 20 + j * (10.0 / (raz - 1));
-            arr_pred[j * raz + (raz - 1)] = 20 + j * (10.0 / (raz - 1));
-        }
-    }
-    cublasHandle_t handle;
-    cublasCreate(&handle);
-    double *dop;
+    double error = 1.0;
+    double shag = 10.0 / (size + 2);
 
-    while (max_num_iter > num_iter && max_toch < error) {
+    int size_pot=32; // количество потоков
+    dim3 Block_size(size_pot, size_pot, 1); //размер блока и определение количества потоков в каждом блоке, 1 блок - 1024 потока
+    dim3 Grid_Size((size + 33)/size_pot, (size + 33)/size_pot, 1); // определяет количество блоков в сетке, рассчитывается
+    // на основе размера матрицы size и используется для распределения блоков на сетке таким образом, чтобы охватить
+    // всю матрицу и иметь достаточное количество блоков для выполнения параллельных вычислений на GPU
+
+    double* arr_pred_gp, *arr_new_gp;
+    cudaMalloc((void**)&arr_pred_gp, sizeof(double ) * (size + 2) * (size + 2));
+    cudaMalloc((void**)&arr_new_gp, sizeof(double ) * (size + 2) * (size + 2));
+
+    int len_host = size + 2;
+
+    for (int i = 0; i < size + 2; i++){
+        arr_pred[i * len_host] = 10 + shag * i;
+        arr_pred[i] = 10 + shag * i;
+        arr_pred[len_host * (size + 1) + i] = 20 + shag * i;
+        arr_pred[len_host * i + size + 1] = 20 + shag * i;
+
+        arr_new[len_host * i] = arr_pred[i * len_host];
+        arr_new[i] = arr_pred[i];
+        arr_new[len_host * (size + 1) + i] = arr_pred[len_host * (size + 1) + i];
+        arr_new[len_host * i + size + 1] = arr_pred[len_host * i + size + 1];
+    }
+
+    double* mas_error;
+    cudaMalloc(&mas_error, sizeof(double) * (size * size)); // выделение памяти для GPU
+    // копирование данных из хоста на устройство
+    cudaMemcpy(arr_pred_gp, arr_pred, sizeof(double) * (size + 2) * (size + 2), cudaMemcpyHostToDevice);
+    cudaMemcpy(arr_new_gp, arr_new, sizeof(double) * (size + 2) * (size + 2), cudaMemcpyHostToDevice);
+    
+
+    dim3 Error_block(1024,1,1);
+    dim3 errGS(ceil((size * size)/(float)errBS.x), 1, 1);
+    double* dev_out;
+    cudaMalloc(&dev_out, sizeof(double) * errGS.x);
+
+
+    double* d_ptr;
+    cudaMalloc((void **)(&d_ptr), sizeof(double));
+
+    cudaDeviceSynchronize();
+    while ((error > tol) && (num_iter < iter_max)){
         num_iter++;
-        if (num_iter % 100 == 0 || num_iter == 1) {
-            
-//объявляется область данных, которые находятся на устройстве
-#pragma acc data present(arr_pred[0:raz*raz], arr_new[0:raz*raz])
-            
-//async(1) указывает, что выполнение этого блока должно начаться после выполнения предыдущего блока
-#pragma acc kernels async(1)
-            {
-//директива указывает на то, что следующий цикл for может быть распараллелен, collapse(2) отвечает за то, что оба вложенных цикла могут быть распараллелены
-#pragma acc loop independent collapse(2)
-                for (int i = 1; i < raz - 1; i++) {
-                    for (int j = 1; j < raz - 1; j++) {
-                        arr_pred[i * raz + j] =0.25 * (arr_new[(i + 1) * raz + j] + arr_new[(i - 1) * raz + j] + arr_new[i * raz + j - 1] + arr_new[i * raz + j + 1]);
-                    }
-                }
-            }
-           
-            int max_id = 0; //хранение индекса максимального элемента массива arr_new
-            const double alpha = -1;
-            
-// останавливает выполнение программы, пока не завершатся все ядра, запущенные с использованием async()
-#pragma acc wait
-            
-//директива определяет, что данные массивов находятся и на устройстве, и на хосте, и могут использоваться и изменяться на обоих уровнях
-#pragma acc host_data use_device(arr_pred, arr_new)
-            {
-                
-                cublasDaxpy(handle, raz * raz, &alpha, arr_pred, 1, arr_new, 1); // функция вычисляет значение -1 * arr_pred + arr_new и сохраняет результат в arr_new
-                cublasIdamax(handle, raz * raz, arr_new, 1, &max_id); //находит индекс максимального элемента массива arr_new
-            }
-//копирует один элемент массива arr_new с индексом max_id-1 с устройства на хост
-#pragma acc update self(arr_new[max_id-1:1])
-#pragma acc update self(arr_new[max_id-1:1])
-            
-            error = fabs(arr_new[max_id - 1]);
-#pragma acc host_data use_device(arr_pred, arr_new)
-            cublasDcopy(handle, raz * raz, arr_pred, 1, arr_new, 1); //функция копирует содержимое массива 
-            
-//указывает, что все ранее запланированные ядра и данные, связанные с ускорителем, должны завершить свою работу, прежде чем продолжить выполнение кода на хост-процессоре
-#pragma acc wait(1)
-            printf("Номер итерации: %d, ошибка: %0.8lf\n", num_iter, error);
-            
-
+        if ((num_iter % 150 == 0) || (num_iter == 1)){
+            error = 0.0;
+            updateError<<<Grid_Size, Block_size>>>(arr_pred_gp, arr_new_gp, size, error, mas_error);
+            reduceError<<<errGS, Error_block, (Error_block.x) * sizeof(double)>>>(mas_error, dev_out, size * size);
+            reduceError<<<1, Error_block, (Error_block.x) * sizeof(double)>>>(dev_out, mas_error, errGS.x);
+            cudaMemcpy(&error, &mas_error[0], sizeof(double), cudaMemcpyDeviceToHost);
         }
-        else {
-#pragma acc data present(arr_pred[0:raz*raz], arr_new[0:raz*raz])
-#pragma acc kernels async(1)
-            {
-#pragma acc loop independent collapse(2)
-                for (int i = 1; i < raz - 1; i++) {
-                    for (int j = 1; j < raz - 1; j++) {
-                        arr_pred[i * raz + j] =0.25 * (arr_new[(i + 1) * raz + j] + arr_new[(i - 1) * raz + j] + arr_new[i * raz + j - 1] + arr_new[i * raz + j + 1]);
-                    }
-                }
-            }
+        else
+            updateTemperature<<<Grid_Size, Block_size>>>(arr_pred_gp, arr_new_gp, size);
+        d_ptr = arr_pred_gp;
+        arr_pred_gp = arr_new_gp;
+        arr_new_gp = d_ptr;
+        if ((num_iter % 150 == 0) || (num_iter == 1)){
+            printf("%d : %lf\n", num_iter, error);
+            fflush(stdout);
         }
-        dop = arr_new;
-        arr_new = arr_pred;
-        arr_pred = dop;
-       
     }
-
-     //вывод сетки размером 15*15
-        printf("\n%d grid:\n", num_iter);
-        if (raz==15){
-                for (int i = 0; i < raz; i++) {
-                    for (int j = 0; j < raz; j++) {
-                        printf("%0.2lf ", arr_new[i * raz + j]);
-                    }   
-                printf("\n");
-                }
-            }
-
-    printf("Final result: %d, %0.6lf\n", num_iter, error);
+    printf("%d : %lf\n", num_iter, error);
     clock_t b=clock();
-    double d=(double)(b-a)/CLOCKS_PER_SEC; // переводит в секунды 
+    double d=(double)(b-a)/CLOCKS_PER_SEC; // переводит в секунды
     printf("%.25f время в секундах", d);
-    cublasDestroy(handle); //освобождает ресурсы, связанные с объектом handle
-    free(arr_pred); 
-    free(arr_new);
     return 0;
 }
