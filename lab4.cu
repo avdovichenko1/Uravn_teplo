0ac9cadda2a1e8762d905dd185a518bf4151a843
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-05-18 11:21:22 +0700

    Add files via upload

--- lab4.cu
+++ lab4.cu
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <malloc.h>
 #include <time.h>
+
 #include <cub/cub.cuh>
 #include <cub/block/block_load.cuh>
 #include <cub/block/block_store.cuh>
@@ -11,32 +12,24 @@
 
 
 __global__ void updateTemperature(const double* arr_pred, double* arr_new, int N){
-    //Индекс j вычисляется как произведение номера блока по вертикальной оси (blockIdx.y) на размер блока по вертикальной оси
-    // (blockDim.y),плюс номер потока внутри блока по вертикальной оси (threadIdx.y), что позволяет потокам различных блоков
-    // и потокам внутри одного блока работать с различными строками массива данных, i - аналогично.
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-//проверяется, что индексы j и i находятся внутри диапазона от 1 до N + 1, чтобы исключить обработку граничных элементов массива.
-    if (j > 0 && j < N-1)
-        if (i > 0 && i < N -1)
-            //новое значение элемента массива arr_new[j * (N + 2) + i] вычисляется на основе предыдущего состояния массива
-            // arr_pred,используя формулу теплопроводности
-            arr_new[j * (N) + i] = 0.25 * (arr_pred[(j + 1) * (N) + i] + arr_pred[(j - 1) * (N) + i] +
-                                               arr_pred[j * (N) + i - 1] + arr_pred[j * (N) + i + 1]);
+    int i = blockIdx.x; // размер строки
+    int j = threadIdx.x; // столбца
+    int k = blockDim.x + 1; // блока
+//проверка находится ли элемент массива внутри границы
+   if (j != 0 && j != k - 1)
+        if (i != 0 && i != k - 1)
+            arr_new[i * k + j] = 0.25 * (arr_pred[i*k+j-1] + arr_pred[(i - 1) * k + j] +
+                                               arr_pred[(i+1)*k+j] + arr_pred[i * k + j + 1]);
 }
 
 __global__ void updateError(const double* arr_pred, double* arr_new, int N, double tol, double* tol1){
-
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (j > 0 && j < N -1)
-        if (i > 0 && i < N - 1) {
-            arr_new[j * (N) + i] = 0.25 * (arr_pred[(j + 1) * (N) + i] + arr_pred[(j - 1) * (N) + i] + arr_pred[j * (N) + i - 1] + arr_pred[j * (N) + i + 1]);
-            //Вычисление значения погрешности между новым значением элемента и соответствующим предыдущим значением элемента
-            tol1[i * j - 1] = max(arr_new[j * (N ) + i] - arr_pred[j * (N) + i], tol);
-        };
+   //локальныt индексs элементов в блоке, в котором выполняется поток
+    int i = (blockIdx.x + gridDim.y + blockIdx.y)*blockDim.x+blockDim.y+(threadIdx.x+threadIdx.y*threadIdx.x) / (gridDim.x * blockDim.x);
+    int j = (blockIdx.x + gridDim.y + blockIdx.y)*blockDim.x+blockDim.y+(threadIdx.x+threadIdx.y*threadIdx.x) % (gridDim.y * blockDim.y);
+// проверяется, что индексы не находятся на границах массива
+    if (j != 0 && j != gridDim.x * blockDim.x -1)
+        if (i != 0 && i < gridDim.y * blockDim.y - 1)
+            tol1[i * (gridDim.x * blockDim.x) + j] = abs(arr_new[i*(gridDim.x * blockDim.x) + j]-arr_pred[i*(gridDim.x * blockDim.x) + j]); //вычисление абсолютной разности между элементами массивов
 }
 
 
